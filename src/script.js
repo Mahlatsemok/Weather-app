f38d772cffee91856d00e8e8ceec87d06213001d
Mahlatsemok <mahlatse.mokhabela@gmail.com>
2024-08-10 13:13:45 +0200

    Adde add some change on html , css and on JavaScript added the basic search engine(No API)

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
-let age = prompt("how old are you?");
-
-if (age > 16) {
-    alert("you can drive");
+function searchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
 }
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
