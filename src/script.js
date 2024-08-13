5c8fcfe826e2fa6497994345415bca7f40aea1b8
Mahlatsemok <mahlatse.mokhabela@gmail.com>
2024-08-13 12:44:00 +0200

    added new API

--- src/script.js
+++ src/script.js
@@ -1,25 +1,24 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
 
-    temperatureElement.innerHTML = Math.round(temperature);
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-    let apiKey = "46f591d0fa567980b4e1b4045d01d041";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=46f591d0fa567980b4e1b4045d01d041`;
-    axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "14a07d3do2f97e772d9326602betbeb8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
 
-    searchCity(searchInput.value);
+  searchCity(searchInput.value);
 }
 
 function searchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
-
