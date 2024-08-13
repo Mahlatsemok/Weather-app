29b126d8c84f837373ea40f870a848b52039ba37
Mahlatsemok <mahlatse.mokhabela@gmail.com>
2024-08-13 12:31:43 +0200

    added some change in html and  in script.js added the search functionality using the API

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "46f591d0fa567980b4e1b4045d01d041";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=46f591d0fa567980b4e1b4045d01d041`;
+    axios.get(apiUrl).then(refreshWeather);
+
+    searchCity(searchInput.value);
+}
+
 function searchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
@@ -7,3 +22,4 @@ function searchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
